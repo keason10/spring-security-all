7865d3c063ac43912c2b58638f40561e4ba5c0f8
keason10 <729662970@qq.com>
2019-04-21 21:06:03 +0800

    spring security init

---
+++ keason-web/src/main/java/com/ky/security/keasonapp/KeasonAppApplication.java
@@ -0,0 +1,13 @@
+package com.ky.security.keasonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeasonAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeasonAppApplication.class, args);
+    }
+
+}
